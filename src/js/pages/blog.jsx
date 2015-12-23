7c064603cd31870544819434d79e8e02c361c133
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-12-23 12:10:06 +0100

    Quick fix

--- src/js/pages/blog.jsx
+++ src/js/pages/blog.jsx
@@ -1,31 +1,3 @@
-// research.jsx ---
-
-// Copyright (C) 2015 Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
-
-// Author: Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
-
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-
-// Except as contained in this notice, the name(s) of the above copyright
-// holders shall not be used in advertising or otherwise to promote the sale,
-// use or other dealings in this Software without prior written authorization.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
-
 import React from 'react';
 import _ from 'lodash';
 import { _b } from '../react-utils/react-bem'
@@ -76,8 +48,8 @@ export default class BlogPage extends React.Component {
         }).then( () => {
             debug("Updating Mathjax..");
             MathJax.Hub.Config({tex2jax:{inlineMath:[['$','$'],['\\(','\\)']]}});
-                   MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-            })
+            MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+        })
     }
 
     componentWillReceiveProps(props) {
@@ -101,13 +73,13 @@ export default class BlogPage extends React.Component {
         if(this.state.valid) {
             let c = _.partial(_b, 'post_container');
             return (
-                    <div className={c()} >
-                        <div className={c('post__title')}> {this.state.postData.title} </div>
-                        {subTitle(this.state.postData)}
-                        <div className="post_text">
-                            <div dangerouslySetInnerHTML={{ __html: this.state.postData.markup}}/>
-                        </div>
+                <div className={c()} >
+                    <div className={c('post__title')}> {this.state.postData.title} </div>
+                    {subTitle(this.state.postData)}
+                    <div className="post_text">
+                        <div dangerouslySetInnerHTML={{ __html: this.state.postData.markup}}/>
                     </div>
+                </div>
             );
         } else {
             return (
